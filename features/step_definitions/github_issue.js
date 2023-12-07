ab613cdf04989eec201c0e6b30d5542d032d7452
Edgar <38365682+e-rojas@users.noreply.github.com>
2023-12-07 09:46:25 -0400

    feat(test): safe guard assertion to check response status

--- features/step_definitions/github_issue.js
+++ features/step_definitions/github_issue.js
@@ -9,6 +9,7 @@ Given('I am an anonymous user', async () => {
 When('I rquest a list of issues for the {string} repository', async (repositoryName) => {
     const url = `https://api.github.com/repos/${repositoryName}/${repositoryName}/issues`
     response = await axios.get(url);
+    assert.equal(response.status, 200, `Expected status code 200, but got ${response.status}`);
 });
 Then('I should find at least {string} result', async (requestResult) => {
     const expectMinIssue = parseInt(requestResult, 10);
